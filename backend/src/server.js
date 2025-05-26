612811bf7c7acf71155dafd5849f3310e7317627
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-05-26 16:15:17 +0200

    chore:/database connected successfully

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"
+import { connectDb } from "./Config/db.js";
 const app = express();
 dotenv.config();
 app.use(express.json());
@@ -8,6 +9,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    await connectDb();
     console.log(`server is running on port ${PORT}`)
 });
