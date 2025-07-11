e8b6fa9721c469e741dfa0c49790a654e007f8b2
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-07-11 19:00:40 +0200

    feat:/sharing functionality work in backend

--- backend/src/Routes/userRoutes.js
+++ backend/src/Routes/userRoutes.js
@@ -12,6 +12,7 @@ import { protect } from "../middleware/authenticated.js";
 import multer from 'multer';
 import { addComment, getCarComments } from "../controllers/commentController.js";
 import { subscribeToCar, getSubscriptions, unsubscribeFromCar } from "../controllers/subscibeController.js";
+import { generateShareLinks } from "../controllers/sharingControlller.js";
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 router.post('/signup', upload.single('profile'), signup);
@@ -23,6 +24,7 @@ router.post("/createCar", createCar);
 router.post("/subscribe", protect, subscribeToCar);
 router.get("/subscriptions/:buyerId", protect, getSubscriptions);
 router.delete("/unsubscribe", protect, unsubscribeFromCar);
+router.get("/share/:carId", generateShareLinks);
 
 router.post("/:carId", protect, addComment);
 router.get('/:carId', getCarComments);
