58cbf75618a714624542d6693943f1cb89a4fb43
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-08-01 16:18:54 +0200

    feat/api integrated

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 import cors from "cors"
 import { connectDb } from "./Config/db.js";
 import userRoute from "./Routes/userRoutes.js";
 const app = express();
-dotenv.config();
 app.use(express.json());
 app.use(cors());
 app.use("/user" , userRoute)
